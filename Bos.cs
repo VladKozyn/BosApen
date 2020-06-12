42104af7ed3ff4dcdc612053ad5099d8b557df4b
vladik kozynin <liekesliefhebber2000@hotmail.com>
2020-06-12 10:50:59 +0200

    f

--- Bos.cs
+++ Bos.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BosApen
 {
@@ -13,22 +15,22 @@ namespace BosApen
         public List<Boom> listBomen = new List<Boom>();
         public List<Aap> listApen = new List<Aap>();
         Random random = new Random();
-        public Bos(int minGrooteX, int minGrooteY, int maxGrooteX, int maxGrooteY,int amount)
+        public Bos(int minGrooteX, int minGrooteY, int maxGrooteX, int maxGrooteY, int amountBomen)
         {
             this.minGrooteX = minGrooteX;
             this.minGrooteY = minGrooteY;
             this.maxGrooteX = maxGrooteX;
             this.maxGrooteY = maxGrooteY;
-            for (int i = 0; i < amount; i++)
+            for (int i = 0; i < amountBomen; i++)
             {
-             
+
                 Boom t = new Boom(
                             random.Next(minGrooteX, maxGrooteX),
-                            random.Next(minGrooteY, maxGrooteY),i);
+                            random.Next(minGrooteY, maxGrooteY), i);
                 listBomen.Add(t);
             };
         }
-        public void maakApen(int hoeveelheid,List<string> namen)
+        public void maakApen(int hoeveelheid, List<string> namen)
         {
             for (int i = 0; i < hoeveelheid; i++)
             {
@@ -36,6 +38,53 @@ namespace BosApen
                 listApen.Add(app);
             }
         }
-       
+        private double AfstandTussenTweeBomen(Boom t1, Boom t2)
+        {
+            double d = Math.Sqrt(Math.Pow(t1.x - t2.x, 2) + Math.Pow(t1.y - t2.y, 2));
+            return d;
+        }
+        private double AfstandBerekenenBorder(Boom b)
+        {
+            double d = (new List<double>()
+            {
+                 b.x - minGrooteX,
+                    maxGrooteX - b.x,
+                  b.x - minGrooteY,
+                    maxGrooteY - b.x,
+            }.Min());
+
+            return d;
+        }
+        private Boom KortsteBoom(Aap a)
+        {
+            List<Boom> ZonderBezochteBomen = listBomen.Except(a.bezochteBomen).ToList();
+            Boom dichtsteBoom = ZonderBezochteBomen.OrderBy(ZBB => AfstandTussenTweeBomen(a.currentBoom(), ZBB)).FirstOrDefault();
+            return dichtsteBoom;
+        }
+        public async Task AapSpring(Aap a)
+        {
+            Boom currentBoomMetAap = a.currentBoom();
+
+            Boom kortsteBoom = KortsteBoom(a);
+            double checkBoom = AfstandTussenTweeBomen(a.currentBoom(), kortsteBoom);
+            double checkRand = AfstandBerekenenBorder(a.currentBoom());
+
+            if (checkRand < checkBoom)
+            {   
+                a.currentBoom().zitErEenAapOpDezeBoom = false;
+             
+                Console.WriteLine("Aap:{0} is uit het bos", a.naam);
+                listApen.Remove(a);
+
+            }
+            else
+            {
+                a.currentBoom().zitErEenAapOpDezeBoom = false;
+                a.bezochteBomen.Add(kortsteBoom);
+                a.currentBoom().zitErEenAapOpDezeBoom = true;
+                Console.WriteLine("Aap:{0} BoomId:{1} Coordinaten:({2},{3})", a.naam, a.currentBoom().id, a.currentBoom().x, a.currentBoom().y);
+
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BosApen
 {
@@ -6,7 +8,25 @@ namespace BosApen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Bos> bossen = new List<Bos>();
+            List<string> namen1 = new List<string>() { "marie", "jos" };
+            List<string> namen2 = new List<string>() { "paul", "keesje" };
+            bossen.Add(new Bos(0, 0, 500, 500, 100));
+            bossen[0].maakApen(2, namen1);
+            bossen.Add(new Bos(0, 0, 500, 500, 100));
+            bossen[1].maakApen(2, namen2);
+
+            for (int i = 0; i < bossen.Count; i++)
+            {
+                while (bossen[i].listApen.Any())
+                {
+                    for (int b = 0; b < bossen[i].listApen.Count; b++)
+                    {
+                        bossen[i].AapSpring(bossen[i].listApen[b]);
+                    }
+                }
+            }
+
         }
     }
 }
